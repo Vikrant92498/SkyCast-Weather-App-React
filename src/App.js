27d889327e04e991706643bd7eae2bed772da931
vikrant <vikrantkumar92498@gmail.com>
2023-07-11 17:33:55 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // Update time every second
+
+    return () => {
+      clearInterval(interval); // Clear the interval on component unmount
+    };
+  }, []);
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const today_Date = currentTime.toLocaleDateString('en-US', options);
+  const current_Time = currentTime.toLocaleTimeString('en-US');
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='app-logo'>SkyCast</div>
+        <div className='today'>
+          <div className="day">{today_Date}</div>
+          <div className="time">{current_Time}</div>
+        </div>
       </header>
     </div>
   );
